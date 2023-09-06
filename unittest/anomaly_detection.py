32a80cdeeed39f89f98d98c2f951ef180dfb8b82
Jay Turla <shipcodez@gmail.com>
2023-09-06 23:12:40 +0800

    Create anomaly_detection.py

---
+++ unittest/anomaly_detection.py
@@ -0,0 +1,29 @@
+# @shipcod3
+# This script sets up a simple IDS for a CAN bus by monitoring incoming messages and checking their CAN IDs against a predefined list of allowed IDs. 
+# If a message with an unexpected ID is received, it prints an anomaly detection message.
+
+import can
+
+# Define a list of allowed CAN message IDs
+allowed_ids = [0x100, 0x200, 0x300]
+
+# Create a CAN bus interface object
+can_bus = can.interface.Bus(bustype='socketcan', channel='can0')
+
+def process_can_message(msg):
+    """
+    Process and analyze incoming CAN messages.
+    """
+    if msg.arbitration_id not in allowed_ids:
+        print(f"Anomaly Detected: Unexpected CAN ID - {hex(msg.arbitration_id)}")
+
+# Main loop to monitor the CAN bus
+try:
+    while True:
+        message = can_bus.recv()
+        process_can_message(message)
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Exiting.")
+finally:
+    can_bus.shutdown()
